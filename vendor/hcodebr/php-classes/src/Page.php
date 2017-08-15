eabf4d590d9a6d717da55f639ea162a158413be8
Amanda Bertolai <amandarbertolai@gmail.com>
2017-08-15 13:29:50 -0300

    tela login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@
 		private $tpl;
 		private $options = [];
 		private $defaults = [
+			"header" => true,
+			"footer" => true,
 			"data" => []
 		];
 
@@ -28,7 +30,7 @@
 
 			$this -> setData($this -> options["data"]);
 
-			$this -> tpl -> draw("header");
+			if ($this -> options["header"] === true) $this -> tpl -> draw("header");
 
 		}
 
@@ -49,7 +51,7 @@
 
 		public function __destruct(){
 
-			$this -> tpl -> draw("footer");
+			if ($this -> options["footer"] === true) $this -> tpl -> draw("footer");
 
 		}
 
